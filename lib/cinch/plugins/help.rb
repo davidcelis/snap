76bd4833682182aecb853ff1beef2d07c0e15f66
David Celis <david@davidcelis.com>
2012-08-22 09:30:11 -0700

    Correctly refer to !roll, not !dice.
    Signed-off-by: David Celis <david@davidcelis.com>

--- lib/cinch/plugins/help.rb
+++ lib/cinch/plugins/help.rb
@@ -17,7 +17,7 @@ module Cinch
       def execute(m, command)
         usage = case command
                 when 'choose'              then Cinch::Plugins::Choose::USAGE
-                when 'dice'                then Cinch::Plugins::Dice::USAGE
+                when 'roll'                then Cinch::Plugins::Dice::USAGE
                 when 'disapprove' || 'lod' then Cinch::Plugins::Disapprove::USAGE
                 when 'google'              then Cinch::Plugins::Google::USAGE
                 when 'help'                then Cinch::Plugins::Help::USAGE
@@ -25,7 +25,7 @@ module Cinch
                 when 's'                   then Cinch::Plugins::S::USAGE
                 when 'seen'                then Cinch::Plugins::Seen::USAGE
                 else 
-                  "I respond to the following commands (preceded by a !): choose, dice, disapprove, google, help, karma, lod, lmgtfy, s, seen. For more info, pass a command to !help"
+                  "I respond to the following commands (preceded by a !): choose, roll, disapprove, google, help, karma, lod, lmgtfy, s, seen. For more info, pass a command to !help"
                 end
         m.reply(usage)
       end
