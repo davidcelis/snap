ba727fb356d79c222fbe24d2fc6df5b49fc1472e
David Celis <david@davidcelis.com>
2012-08-22 10:27:40 -0700

    Default dice rolls to 1d20.
    Signed-off-by: David Celis <david@davidcelis.com>

--- lib/cinch/plugins/dice.rb
+++ lib/cinch/plugins/dice.rb
@@ -5,16 +5,19 @@ module Cinch
     class Dice
       include Cinch::Plugin
     
-      USAGE = "Roll some dice. Example: !roll 4d6+3"
+      USAGE = "Roll some dice! Defaults to 1d20. Example: !roll 4d6+3"
 
-      match /roll (?:(\d+))?d(\d+)(?:([+-])(\d+))?/
+      match /roll(?: (?:(\d+))?d(\d+)(?:([+-])(\d+))?)?/
     
-      # Roll one or more dice, RPG style.
+      # Roll one or more dice, RPG style. Default is 1d20.
       #
       # <davidcelis> !roll 2d6+4
       # * snap casts the dice and rolls a 13
       def execute(m, rolls, sides, operator, offset)
         rolls, sides, offset = rolls.to_i, sides.to_i, offset.to_i
+        rolls ||= 1
+        sides ||= 20
+
         score = 0
     
         rolls.times do
