41f96b624df9316e227e5c1141e13b9292322e12
David Celis <david@davidcelis.com>
2012-08-22 09:32:27 -0700

    Convert parameters to ints.
    Signed-off-by: David Celis <david@davidcelis.com>

--- lib/cinch/plugins/dice.rb
+++ lib/cinch/plugins/dice.rb
@@ -14,13 +14,14 @@ module Cinch
       # <davidcelis> !roll 2d6+4
       # * snap casts the dice and rolls a 13
       def execute(m, rolls, sides, operator, offset)
+        rolls, sides, offset = rolls.to_i, sides.to_i, offset.to_i
         score = 0
     
-        rolls.to_i.times do
-          score += rand(sides.to_i)
+        rolls.times do
+          score += rand(sides)
         end
     
-        score.send(operator, offset.to_i) if operator && offset
+        score.send(operator, offset) if operator && offset
     
         inflection = sides > 1 ? 'dice' : 'die'
         m.channel.action "casts the #{inflection} and rolls a #{score}", true
